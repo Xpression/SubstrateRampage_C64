a1c6e2549e90eecf7e1f63d64daa7082544b9349
Jan-Ove Karlberg <jakarl@microsoft.com>
2023-09-12 18:57:26 +0200

    Add simple support for reading input from Joystick 2. Game loop scaffolding for acting on input

--- game.asm
+++ game.asm
@@ -3,15 +3,46 @@ game:
 	lda #$00
 	sta $d020
 
-	// Check if joystick pressed
-	lda joystick_buf
-	and #$10 // Only care about the fifth bit (fire button)
-	cmp #$10
-	beq game
+	
+// Section for checking input and acting on it
+chk_f_pressed:
+	// Check if fire button pressed
+	jsr fire_pressed
+	lda joystick_fire
+	cmp #$ff
+	bne chk_l_pressed
+	jsr handle_fire_pressed
 
-	// set border green color only if joystisk hit
-	lda #$05
-	sta $d020
+chk_l_pressed:
+	jsr left_pressed
+	lda joystick_left
+	cmp #$ff
+	bne chk_r_pressed
+	jsr handle_left_pressed
+
+chk_r_pressed:
+	jsr right_pressed
+	lda joystick_right
+	cmp #$ff
+	bne chk_d_pressed
+	jsr handle_right_pressed
+
+chk_d_pressed:
+	jsr down_pressed
+	lda joystick_down
+	cmp #$ff
+	bne chk_u_pressed
+	jsr handle_down_pressed
+
+chk_u_pressed:
+	jsr up_pressed
+	lda joystick_up
+	cmp #$ff
+	bne input_check_complete
+	jsr handle_up_pressed
+
+input_check_complete:
+// Section end
 
 	ldx #$ff
 wait:
@@ -22,3 +53,31 @@ wait:
 
 game_over:
 	jmp menu
+
+
+// Routines for acting on input from Joystick
+handle_fire_pressed:
+	lda #$05 // Green
+	sta $d020
+	rts
+
+handle_left_pressed:
+	lda #$06 // blue
+	sta $d020
+	rts
+
+handle_right_pressed:
+	lda #$07 // yellow
+	sta $d020
+	rts
+
+handle_up_pressed:
+	lda #$08 // orange
+	sta $d020
+	rts
+
+handle_down_pressed:
+	lda #$04 // purple
+	sta $d020
+	rts
+
--- input.asm
+++ input.asm
@@ -1,4 +1,9 @@
 .label joystick_buf = $5000
+.label joystick_fire = $5001
+.label joystick_left = $5002
+.label joystick_right = $5003
+.label joystick_up = $5004
+.label joystick_down = $5004
 
 // this subroutine gets called during IRQ setup, with interrupts disabled
 input_init:
@@ -10,4 +15,94 @@ input_init:
 input_irq:
 	lda $dc00
 	sta joystick_buf
+	rts
+
+// Checks whether fire is pressed. Populates joystick_fire with 0xff if fire button hit, 0x00 otherwise
+fire_pressed:
+
+	lda #$00
+	sta joystick_fire
+
+	lda joystick_buf
+	and #$10 // Only care about the fifth bit (fire button)
+	cmp #$10
+	beq fret
+
+fpressed:
+	lda #$ff
+	sta joystick_fire
+
+fret:
+	rts
+
+// Checks whether left is pressed. Populates joystick_left with 0xff if left button hit, 0x00 otherwise
+left_pressed:
+
+	lda #$00
+	sta joystick_left
+
+	lda joystick_buf
+	and #$04 // Only care about the fifth bit (fire button)
+	cmp #$04
+	beq lret
+
+lpressed:
+	lda #$ff
+	sta joystick_left
+
+lret:
+	rts
+
+// Checks whether right is pressed. Populates joystick_right with 0xff if right button hit, 0x00 otherwise
+right_pressed:
+
+	lda #$00
+	sta joystick_right
+
+	lda joystick_buf
+	and #$08 // Only care about the fifth bit (fire button)
+	cmp #$08
+	beq rret
+
+rpressed:
+	lda #$ff
+	sta joystick_right
+
+rret:
+	rts
+
+// Checks whether up is pressed. Populates joystick_up with 0xff if up button hit, 0x00 otherwise
+up_pressed:
+
+	lda #$00
+	sta joystick_up
+
+	lda joystick_buf
+	and #$01 // Only care about the fifth bit (fire button)
+	cmp #$01
+	beq uret
+
+upressed:
+	lda #$ff
+	sta joystick_up
+
+uret:
+	rts
+
+// Checks whether down is pressed. Populates joystick_down with 0xff if down button hit, 0x00 otherwise
+down_pressed:
+
+	lda #$00
+	sta joystick_down
+
+	lda joystick_buf
+	and #$02 // Only care about the fifth bit (fire button)
+	cmp #$02
+	beq dret
+
+dpressed:
+	lda #$ff
+	sta joystick_down
+
+dret:
 	rts
