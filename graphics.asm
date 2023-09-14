ca06d6416144740b6e0fd91fd616ff96aeaa6d73
Jan-Ove Karlberg <jakarl@microsoft.com>
2023-09-14 21:20:40 +0200

    add code that supports placing sprites full x-width

--- graphics.asm
+++ graphics.asm
@@ -71,6 +71,238 @@
 	.byte %10010000,%01111100,%00010010
 	.byte %11111111
 
+	// Sprite four at $30c0			
+	.byte %00001111,%11111111,%11100000
+	.byte %00011111,%11111111,%11110000
+	.byte %00111111,%11111111,%11111000
+	.byte %00110111,%11111111,%11011000
+	.byte %01110011,%11111111,%10011100
+	.byte %01100001,%11111111,%00001100
+	.byte %01100000,%11111110,%00001100
+	.byte %01100000,%11111110,%00001100
+	.byte %01110000,%01111100,%00011100
+	.byte %00110000,%00111000,%00011000
+	.byte %00011100,%01111100,%01110000
+	.byte %00000111,%11101111,%11000000
+	.byte %00000011,%11000111,%10000000
+	.byte %00000001,%11111111,%00000000
+	.byte %00000000,%11111110,%00000000
+	.byte %00000000,%10101010,%00000000
+	.byte %00111100,%00000000,%01111000
+	.byte %01001010,%10101010,%10100100
+	.byte %10010010,%11111110,%10010010
+	.byte %10010010,%11111110,%10010010
+	.byte %10010000,%01111100,%00010010
+	.byte %11111111
+
+// These should be set if we exceed x == 255
+sprite_x_overflow_flags:
+	.byte 0, 0, 0, 0, 0, 0, 0, 0
+
+// Some buffers for working with x-coordinates
+sprite_x_hi:
+	.byte 0
+sprite_x_lo:
+	.byte 0
+
+// Subroutine that increments sets a sprites x-position.
+// 'sprite_num_buf' contains the 0-indexed sprite number [0-7]
+// 'sprite_x_hi' contains the high byte, will always be 0x01 or 0x00
+// 'sprite_x_lo' contains the low byte, will range 0x00 to 0xff
+set_sprite_x_position:
+
+	// if the high byte != 0x00, set the overflow flag for the given sprite
+	lda sprite_x_hi
+	cmp #$0
+	beq no_overflow
+
+	ldx sprite_num_buf
+	lda #$01
+	sta sprite_x_overflow_flags, x
+
+	// also set the extra bit that the VIC II uses for this sprite (0xD010)
+	jsr set_vic_overflow
+
+	// and then continue with the lsbs
+	jmp ssxp_lsb
+
+
+no_overflow:
+	// Clear the overflow flag
+	ldx sprite_num_buf
+	lda #$00
+	sta sprite_x_overflow_flags, x
+
+	// Clear the extra bit the VIC II uses for this rpite (0xD010)
+	jsr clear_vic_overflow
+
+ssxp_lsb:
+
+	// This is where we set the LSB for the sprite
+	lda sprite_num_buf
+	asl // multiply by two
+	tax 
+
+	// Store the LSB 
+	lda sprite_x_lo
+	sta $d000, x
+
+ssxp_exit:
+	rts
+
+// 'sprite_num_buf' contains the 0-indexed sprite number [0-7]
+set_vic_overflow:
+	lda sprite_num_buf
+	cmp #$00
+	bne sb_one
+
+	lda $d010
+	ora #%00000001
+	sta $d010
+	jmp svo_exit
+
+sb_one:
+	cmp #$01
+	bne sb_two
+
+	lda $d010
+	ora #%00000010
+	sta $d010
+	jmp svo_exit
+
+sb_two:
+	cmp #$02
+	bne sb_three
+
+	lda $d010
+	ora #%00000100
+	sta $d010
+	jmp svo_exit
+
+sb_three:
+	cmp #$03
+	bne sb_four
+
+	lda $d010
+	ora #%00001000
+	sta $d010
+	jmp svo_exit
+
+sb_four:
+	cmp #$04
+	bne sb_five
+
+	lda $d010
+	ora #%00010000
+	sta $d010
+	jmp svo_exit
+
+sb_five:
+	cmp #$05
+	bne sb_six
+
+	lda $d010
+	ora #%00100000
+	sta $d010
+	jmp svo_exit
+
+sb_six:
+	cmp #$06
+	bne sb_seven
+
+	lda $d010
+	ora #%01000000
+	sta $d010
+	jmp svo_exit
+
+sb_seven:
+	cmp #$07
+	bne svo_exit
+
+	lda $d010
+	ora #%10000000
+	sta $d010
+
+svo_exit:
+	rts
+
+// 'sprite_num_buf' contains the 0-indexed sprite number [0-7]
+clear_vic_overflow:
+	lda sprite_num_buf
+	cmp #$00
+	bne cb_one
+
+	lda $d010
+	and #%11111110
+	sta $d010
+	jmp cvo_exit
+
+cb_one:
+	cmp #$01
+	bne cb_two
+
+	lda $d010
+	and #%11111101
+	sta $d010
+	jmp cvo_exit
+
+cb_two:
+	cmp #$02
+	bne cb_three
+
+	lda $d010
+	and #%11111011
+	sta $d010
+	jmp cvo_exit
+
+cb_three:
+	cmp #$03
+	bne cb_four
+
+	lda $d010
+	and #%11110111
+	sta $d010
+	jmp cvo_exit
+
+cb_four:
+	cmp #$04
+	bne cb_five
+
+	lda $d010
+	and #%11101111
+	sta $d010
+	jmp cvo_exit
+
+cb_five:
+	cmp #$05
+	bne cb_six
+
+	lda $d010
+	and #%11011111
+	sta $d010
+	jmp cvo_exit
+
+cb_six:
+	cmp #$06
+	bne cb_seven
+
+	lda $d010
+	and #%10111111
+	sta $d010
+	jmp cvo_exit
+
+cb_seven:
+	cmp #$07
+	bne cvo_exit
+
+	lda $d010
+	and #%01111111
+	sta $d010
+
+cvo_exit:
+	rts
+
+
 init_sprite_one:
 	// Set pointer to sprite one data
 	lda #$0c0 // 0x3000 / 0x40 => 0xc0
@@ -110,7 +342,7 @@ init_sprite_three:
 	lda #$0c2 // 0x3080 / 0x40 => 0xc2
 	sta sprite_three
 
-	// Enable sprite two
+	// Enable sprite three
 	lda $d015
 	ora #%00000100
 	sta $d015 // sprite enable register
@@ -119,6 +351,32 @@ init_sprite_three:
 	lda #$b0 	
 	sta $d004	// sprite two position x
 	sta $d005	// sprite two position y
+
+	rts
+
+init_sprite_four:
+	// Set pointer to two one data
+	lda #$0c3 // 0x30c0 / 0x40 => 0xc3
+	sta sprite_four
+
+	// Enable sprite four
+	lda $d015
+	ora #%00001000
+	sta $d015 // sprite enable register
+
+	// Set position of sprite four (zero-indexed)
+	// x-coordina - overflow
+	lda #$03
+	sta sprite_num_buf
+	lda #$40 
+	sta sprite_x_lo
+	lda #$01
+	sta sprite_x_hi
+	jsr set_sprite_x_position
+
+	// y-coordinate
+	lda #$b0
+	sta $d007	// sprite two position y
 
 	rts
 
